ab75188929dfcb83bcee830d546bd665ff754755
thomas.ricciardiello <thomas.ricciardiello@uts.edu.au>
2018-07-24 17:58:28 +1000

    Removed test file.

--- test.js
+++
@@ -1,6 +0,0 @@
-const moment = require('moment-timezone');
-
-var deadline = moment(moment("20181015").tz("Australia/Sydney").format());
-var now = moment(moment().tz("Australia/Sydney").format());
-
-console.log(deadline.diff(now, "days"));
