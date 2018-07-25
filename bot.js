1cccac7084d9de521b85d77ff370253f292df68f
thomas.ricciardiello <thomas.ricciardiello@uts.edu.au>
2018-07-26 07:43:58 +1000

    Added hashtag to tweet.

--- bot.js
+++ bot.js
@@ -15,9 +15,9 @@ let deadline = moment(moment("20181015").tz("Australia/Sydney").format());
 let now = moment(moment().tz("Australia/Sydney").format());
 let remaining = deadline.diff(now, "days");
 if (remaining === 1) {
-  phrase = remaining + " day left to opt out.";
+  phrase = remaining + " day left to opt out of #MyHealthRecord.";
 } else {
-  phrase = remaining + " days left to opt out.";
+  phrase = remaining + " days left to opt out of #MyHealthRecord";
 }
 
 Bot.tweet(phrase);
